c884588668a418525e20350490763152ac51f9a2
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-04 20:41:25 -0400

    References Favorite in User model

--- server/models/User.js
+++ server/models/User.js
@@ -25,6 +25,12 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
+  favorites: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Favorite"
+    }
+  ]
 });
 
 // set up pre-save middleware to create password
