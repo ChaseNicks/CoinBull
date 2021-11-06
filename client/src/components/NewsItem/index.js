c2a4d89cac1ef03be58d50b300c5b355bdc19f0f
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-06 13:50:30 -0400

    Further edits newsCard styling

--- client/src/components/NewsItem/index.js
+++ client/src/components/NewsItem/index.js
@@ -3,12 +3,16 @@ import React from "react";
 const NewsItem = (props) => {
   return (
     <div className="news-item">
-      <img src={props.image} alt={props.title} className="news-image" />
-      <h5 className="title is-5">{props.title}</h5>
-      <p>{props.description}</p>
-      <a href={props.url} rel="noreferrer" target="_blank">
-        Go to Article
-      </a>
+      <div className="image-container">
+        <img src={props.image} alt={props.title} className="news-image" />
+      </div>
+      <div className="item-content">
+        <h5 className="title is-5 news-title">{props.title}</h5>
+        <p>{props.description}</p>
+        <a href={props.url} rel="noreferrer" target="_blank">
+          Go to Article
+        </a>
+      </div>
     </div>
   );
 };
