dcd12a9ec6e46cb32828a8f1a44294c511492b23
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-06 11:09:31 -0400

    Adds axios call for lunarcrush news

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,3 +6,11 @@ export async function getAllCoins() {
   const coins = res.data;
   return coins;
 }
+
+// Gets news from Lunarcrush
+const newsURL = `https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&limit=10&sources=news`;
+export async function getNews() {
+  const res = await axios.get(newsURL);
+  const news = res.data;
+  return news;
+}
