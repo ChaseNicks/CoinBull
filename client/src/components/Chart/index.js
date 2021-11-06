57bcce9955cff86192c6de94e734915e90984503
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-11-06 13:57:41 -0400

    Added the chart for a single coin

--- client/src/components/Chart/index.js
+++ client/src/components/Chart/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import chart from "../../assets/chart.png";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
-const index = ({ singleCoin }) => {
+const Chart = ({ singleCoin }) => {
   return (
     <div>
-      <img width="600" src={chart} alt="coin chart" />
+      <TradingViewWidget
+        symbol={singleCoin + "USD"}
+        theme={Themes.DARK}
+        width={700}
+        height={500}
+        local="en"
+      />
+
       {console.log(singleCoin)}
     </div>
   );
 };
 
-export default index;
+export default Chart;
--- client/src/pages/SingleCoin.js
+++ client/src/pages/SingleCoin.js
@@ -2,25 +2,28 @@ import React, { useEffect, useState } from "react";
 import { getASingleCoin } from "../utils/API";
 import Chart from "../components/Chart";
 import ProductList from "../components/ProductList";
+import { useParams } from "react-router-dom";
 
 const SingleCoin = () => {
-  const [singleCoin, setSingleCoin] = useState("");
+  // const [singleCoin, setSingleCoin] = useState("");
 
-  useEffect(() => {
-    const fetchASingleCoin = async () => {
-      try {
-        const coin = await getASingleCoin();
-        setSingleCoin(coin);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetchASingleCoin();
-  }, []);
+  // useEffect(() => {
+  //   const fetchASingleCoin = async () => {
+  //     try {
+  //       const coin = await getASingleCoin();
+  //       setSingleCoin(coin);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+  //   fetchASingleCoin();
+  // }, []);
+
+  const { id } = useParams();
 
   return (
     <div>
-      <Chart singleCoin={singleCoin} />
+      <Chart singleCoin={id} />
       {/*  <News /> */}
       <ProductList />
     </div>
