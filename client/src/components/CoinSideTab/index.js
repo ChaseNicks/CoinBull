595fa52d5cd7149fd0603bb569e813cd0c59771e
Chase Nicks <chasenicks81@gmail.com>
2021-11-08 17:43:03 -0500

    converting decimals into percent for coin tabs

--- client/src/components/CoinSideTab/index.js
+++ client/src/components/CoinSideTab/index.js
@@ -4,6 +4,7 @@ const CoinSideTab = (coin) => {
   let { name, id, logo_url, price, change, circulating_supply, market_cap } =
     coin;
   price = parseFloat(price).toFixed(4);
+  change = parseFloat(change * 100).toFixed(2);
   // circulating_supply = circulating_supply.replace(/(.)(?=(\d{3})+$)/g, "$1,");
   // market_cap = market_cap.replace(/(.)(?=(\d{3})+$)/g, "$1,");
 
@@ -23,7 +24,7 @@ const CoinSideTab = (coin) => {
             : "has-text-danger is-vcentered"
         }
       >
-        {change}
+        {change} %
       </th>
     </tr>
   );
