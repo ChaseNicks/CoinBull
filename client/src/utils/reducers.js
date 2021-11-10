791952ff946f18298655de32ec2ccf8e8b382f9e
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-11-10 11:45:33 -0500

    Added the favorites to the GlobalState and the reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,6 +1,6 @@
 import { useReducer } from "react";
 import {
-  UPDATE_PRODUCTS,
+  UPDATE_FAVORITES,
   ADD_TO_CART,
   UPDATE_CART_QUANTITY,
   REMOVE_FROM_CART,
@@ -15,10 +15,10 @@ import {
 export const reducer = (state, action) => {
   switch (action.type) {
     // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
-    case UPDATE_PRODUCTS:
+    case UPDATE_FAVORITES:
       return {
         ...state,
-        products: [...action.products],
+        favorites: [...action.favorites],
       };
 
     case ADD_TO_CART:
