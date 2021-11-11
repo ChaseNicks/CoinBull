abf2783f5350cb39ef4080fe37129b7d86b3601a
Chase Nicks <chasenicks81@gmail.com>
2021-11-11 14:58:18 -0500

    mobile view adjustments with home and footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,22 +2,23 @@ import React from "react";
 import pricesPhoto from '../assets/chart.png';
 import pricingPhoto from '../assets/prices.png';
 import favPhoto from '../assets/favs.png';
+import newsPhoto from '../assets/newsss.png';
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="container" style={{ marginTop: "2rem" }}>
+    <div className="container backgroundbull" style={{ marginTop: "2rem" }}>
       {/* <CategoryMenu /> */}
       {/* <ProductList />
       <NewsCard symbol="ADA" /> */}
-      <h1 className="is-size-1 has-text-centered has-text-weight-bold" style={{ marginTop: "1.2rem", marginBottom: "2.5rem"}}> Created & Designed For You</h1>
-      <div className="columns is-pricing" style={{ marginRight: "2rem", marginLeft: "2rem" }}>
+      <h1 className="is-size-1 is-size-2-mobile has-text-centered has-text-weight-bold" style={{ marginTop: "2.5rem", marginBottom: "2.5rem" }}> Designed & Created For You</h1>
+      <div className="columns is-pricing" style={{ marginRight: "1rem", marginLeft: "1rem", marginTop: "2.5rem" }}>
         <div className="column is-half is-flex-direction-column is-align-items-center">
-          <h1 className="is-size-2 has-text-weight-medium">Techincal Chart with Multiple Features and Studies</h1>
-          <p style={{ marginTop: "1.3rem" }} className="is-size-5">Responsive charts that gives you the power to make your best analysis from high level all the way to the lowest level on granularity </p>
+          <h1 className="is-size-2 is-size-3-mobile has-text-weight-medium">Technical Chart with Multiple Features and Studies</h1>
+          <p style={{ marginTop: "1.3rem" }} className="is-size-5 is-size-6-mobile">Responsive charts that gives you the power to make your best analysis from high level all the way to the lowest level on granularity </p>
           <div className="is-flex is-justify-content-center">
-            <Link to="/charts" style={{ textDecoration: "none", fontSize: "1.2rem" }}><button
-              className="button is-block is-fullwidth is-primary is-medium is-rounded"
+            <Link to="/charts" style={{ textDecoration: "none" }}><button
+              className="button is-block is-size-5 is-size-6-mobile is-fullwidth is-primary is-medium is-rounded"
               type="submit"
               style={{
                 backgroundColor: "rgb(56, 200, 56)",
@@ -35,20 +36,20 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="is-flex-direction-column is-justify-content-center is-align-content-center" style={{ marginRight: "2rem", marginLeft: "2rem" }}>
+      <div className="is-flex-direction-column is-justify-content-center is-align-content-center" style={{ marginRight: "1rem", marginLeft: "1rem", marginTop: "2.5rem" }}>
 
         <div className=" is-flex-direction-column is-align-items-center">
-          <h1 className="is-size-2 has-text-weight-medium has-text-centered">Real Time Pricing For Your Favorite Coins</h1>
-          <p style={{ marginTop: "1.3rem" }} className="is-size-5 has-text-centered">Dynamic table with endless amounts of coins with all the data you could ever need to make better buying and selling decisions. Filter and sort by the data points thats going to help you lock in your future for the digital world.</p>
+          <h1 className="is-size-2 is-size-3-mobile  has-text-weight-medium has-text-centered">Real Time Pricing For Your Favorite Coins</h1>
+          <p style={{ marginTop: "1.3rem" }} className="is-size-5 is-size-6-mobile has-text-centered">Dynamic table with endless amounts of coins with all the data you could ever need to make better buying and selling decisions. Filter and sort by the data points thats going to help you lock in your future for the digital world.</p>
 
         </div>
         <div style={{ marginTop: "1.3rem" }} className="is-flex is-justify-content-center">
           <img src={pricingPhoto} alt="chart" />
         </div>
         <div className="is-flex is-justify-content-center">
-        <Link to="/prices" style={{ textDecoration: "none", fontSize: "1.2rem" }}>
-          <button
-              className="button is-block is-fullwidth is-primary is-medium is-rounded"
+          <Link to="/prices" style={{ textDecoration: "none" }}>
+            <button
+              className="button is-block is-fullwidth is-size-5 is-size-6-mobile is-primary is-medium is-rounded"
               type="submit"
               style={{
                 backgroundColor: "rgb(56, 200, 56)",
@@ -58,32 +59,46 @@ const Home = () => {
                 width: "15rem"
               }}
             >View</button>
-            </Link>
-          </div>
+          </Link>
+        </div>
       </div>
 
-      <div className="columns is-pricing" style={{ marginRight: "2rem", marginLeft: "2rem", marginTop: "1.3rem" }}>
+      <div className="columns is-pricing" style={{ marginRight: "1rem", marginLeft: "1rem", marginTop: "2.5rem", marginBottom: "2.5rem" }}>
         <div className="column is-half is-flex-direction-column is-align-items-center">
-          <h1 className="is-size-2 has-text-weight-medium">A Page Tailored Just For You</h1>
-          <p style={{ marginTop: "1.3rem" }} className="is-size-5"> You tracking a particular coin or maybe even multiple? Favorite it so you can view all your favorite coins on the same page with chart, pricing, % change, and more.</p>
+          <h1 className="is-size-2 is-size-3-mobile has-text-weight-medium">A Page Tailored Just For You</h1>
+          <p style={{ marginTop: "1.3rem" }} className="is-size-5 is-size-6-mobile"> You tracking a particular coin or maybe even multiple? Favorite it so you can view all your favorite coins on the same page with chart, pricing, % change, and more.</p>
           <div className="is-flex is-justify-content-center">
-          <Link to="/favorites" style={{ textDecoration: "none", fontSize: "1.2rem" }}>
-            <button
-              className="button is-block is-fullwidth is-primary is-medium is-rounded"
-              type="submit"
-              style={{
-                backgroundColor: "rgb(56, 200, 56)",
-                marginTop: "1.3rem",
-                marginBottom: "1.3rem",
-                marginRight: "2rem",
-                width: "15rem"
-              }}
-            > Favorites</button>
+            <Link to="/favorites" style={{ textDecoration: "none" }}>
+              <button
+                className="button is-block is-fullwidth is-size-5 is-size-6-mobile is-primary is-medium is-rounded"
+                type="submit"
+                style={{
+                  backgroundColor: "rgb(56, 200, 56)",
+                  marginTop: "1.3rem",
+                  marginBottom: "1.3rem",
+                  marginRight: "2rem",
+                  width: "15rem"
+                }}
+              > Favorites</button>
             </Link>
           </div>
         </div>
         <div className="column is-half">
           <img src={favPhoto} alt="chart" />
+        </div>
+      </div>
+
+      <div className="is-flex-direction-column is-justify-content-center is-align-content-center" style={{ marginRight: "1rem", marginLeft: "1rem", marginTop: "2.5rem", marginBottom: "2.5rem" }}>
+
+        <div className=" is-flex-direction-column is-align-items-center">
+          <h1 className="is-size-2 is-size-3-mobile  has-text-weight-medium has-text-centered">Always Know Whats Being Talked About</h1>
+          <p style={{ marginTop: "1.3rem" }} className="is-size-5 is-size-6-mobile has-text-centered">Constent updating news feed so you'll always be on top of whats happening in the crypto world. Make better educated decisions based on what is being talked about in the media.</p>
+
+        </div>
+        <div style={{ marginTop: "1.3rem" }} className="is-flex is-justify-content-center">
+          <img src={newsPhoto} alt="chart" />
+        </div>
+        <div className="is-flex is-justify-content-center">
         </div>
       </div>
 
