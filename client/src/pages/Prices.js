34e1ebb0ba210c3351e5f58b69b5f53cac8e2304
Leland Hayes <lelandkhayes13@gmail.com>
2021-11-10 20:56:26 -0500

    added all components for the pricing page

--- client/src/pages/Prices.js
+++ client/src/pages/Prices.js
@@ -1,9 +1,16 @@
 import React from "react";
+import ProductList from "../components/ProductList";
+// import CategoryMenu from "../components/CategoryMenu";
+import NewsCard from "../components/NewsCard";
 
-const Prices = () => {
-    return (
-      <h2>Prices go here</h2>
-    );
-  };
-  
-  export default Prices;
+const Home = () => {
+  return (
+    <div className="container">
+      {/* <CategoryMenu /> */}
+      <ProductList />
+      <NewsCard symbol="BTC, ETH, SOL, ADA" />
+    </div>
+  );
+};
+
+export default Home;
