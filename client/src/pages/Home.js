8d40e139fdbeb04d578b72ec9810abc0c68906d5
Will Berner <will.o.berner@gmail.com>
2021-11-10 20:56:01 -0500

    Fixed merge conflict.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ProductList from "../components/ProductList";
-// import CategoryMenu from "../components/CategoryMenu";
 import NewsCard from "../components/NewsCard";
 
 const Home = () => {
   return (
     <div className="container">
-      {/* <CategoryMenu /> */}
       <ProductList />
       <NewsCard symbol="ADA" />
     </div>
