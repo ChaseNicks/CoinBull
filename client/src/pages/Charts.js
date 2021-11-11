7afe2e2f2ed63c429d0186c60c85db948c116ea7
Leland Hayes <lelandkhayes13@gmail.com>
2021-11-11 10:43:52 -0500

    made chart larger to fit full width of page

--- client/src/pages/Charts.js
+++ client/src/pages/Charts.js
@@ -1,22 +1,17 @@
 import React from "react";
-import Chart from "../components/Chart";
-import { useParams } from "react-router-dom";
 import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
-const Charting = (singleCoin) => {
-
-// const { id } = useParams();
+const Charting = () => {
 
   return (
     <div>
       <div className="is-flex is-justify-content-center">
-        {/* <Chart singleCoin={id} />  */}
         <TradingViewWidget
         container_id = "techincal-analysis"
-        symbol={singleCoin + "USD"}
-        theme={Themes.WHITE}
-        width={1800}
-        height={900}
+        symbol={"BTC" + "USD"}
+        theme={Themes.DARK}
+        width={1400}
+        height={700}
         local="en"
         toolbar_bg= "#f1f3f6"
       />       
