b95b421c7a13870f56ff7f787742f3047f5389d8
Will Berner <will.o.berner@gmail.com>
2021-11-15 21:31:27 -0500

    Added security to JWTs with dotenv.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,6 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Category {
-    _id: ID
-    name: String
-  }
-
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
-
-  type Order {
-    _id: ID
-  }
 
   type Favorite {
     _id: ID
@@ -39,23 +21,14 @@ const typeDefs = gql`
     favorites: [Favorite]
   }
 
-  type Checkout {
-    session: ID
-  }
-
   type Auth {
     token: ID
     user: User
   }
 
   type Query {
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
     user: User
     users: [User]
-    order(_id: ID!): Order
-    checkout(products: [ID]!): Checkout
     getFavoriteCoins: User
   }
 
@@ -68,14 +41,12 @@ const typeDefs = gql`
     ): Auth
     addFavorite(input: favorite!): User
     removeCoinFromFavorite(name: String!): User
-    addOrder(products: [ID]!): Order
     updateUser(
       firstName: String
       lastName: String
       email: String
       password: String
     ): User
-    updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
   }
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretsshhhhh";
+const secret = process.env.JWT_SECRET;
 const expiration = "2h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
+    // Allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
